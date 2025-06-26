80d1e68bf920c955883d54448df5b2ea13f95afc
Никита Теслюк <nikiteslyuk@yandex.ru>
2025-06-26 13:39:23 +0300

    first po comand released

--- dodo.py
+++ dodo.py
@@ -6,10 +6,21 @@ from shutil import rmtree
 PODEST = "chessclub/locales"
 DOIT_CONFIG = {"default_tasks": ["docs"]}
 
+
 def task_pot():
     """Re-create .pot ."""
     return {
         "actions": [f"pybabel extract -o CHESS.pot chessclub"],
         "file_dep": [*Path(".").glob("*.py")],
         "targets": ["CHESS.pot"],
+    }
+
+
+# разкомментировать в случае экстренной инициализации перевода
+def task_po():
+    """Init translations."""
+    return {
+        "actions": [f"pybabel init -D CHESS -d {PODEST} -l ru_RU.UTF-8 -i CHESS.pot"],
+        "file_dep": ["CHESS.pot"],
+        "targets": [f"CHESS.po"],
     }
