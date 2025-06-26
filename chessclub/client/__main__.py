faf813e4e4a9df56bc319745fffc322af914e08a
Никита Теслюк <nikiteslyuk@yandex.ru>
2025-06-26 12:21:03 +0300

    Started main for client

---
+++ chessclub/client/__main__.py
@@ -0,0 +1,13 @@
+"""Client for chess."""
+import socket
+import pickle
+import cmd
+import shlex
+import sys
+import time
+import pygame
+import os
+import chess
+import gettext
+import locale as loc
+import readline
