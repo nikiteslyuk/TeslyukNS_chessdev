1ef0b7af696d472bdefc6746fdf35f01b94c3628
Никита Теслюк <nikiteslyuk@yandex.ru>
2025-06-26 13:38:51 +0300

    pot comand released

--- dodo.py
+++ dodo.py
@@ -5,3 +5,11 @@ from shutil import rmtree
 
 PODEST = "chessclub/locales"
 DOIT_CONFIG = {"default_tasks": ["docs"]}
+
+def task_pot():
+    """Re-create .pot ."""
+    return {
+        "actions": [f"pybabel extract -o CHESS.pot chessclub"],
+        "file_dep": [*Path(".").glob("*.py")],
+        "targets": ["CHESS.pot"],
+    }
