aa0242d88e7aece190a5b09f881438d9179702fa
Никита Теслюк <nikiteslyuk@yandex.ru>
2025-06-26 12:21:41 +0300

    Added some params

--- chessclub/client/__main__.py
+++ chessclub/client/__main__.py
@@ -11,3 +11,17 @@ import chess
 import gettext
 import locale as loc
 import readline
+
+if 'libedit' in readline.__doc__:
+    readline.parse_and_bind("bind ^I rl_complete")
+else:
+    readline.parse_and_bind("tab: complete")
+
+SERVER = "127.0.0.1"
+PORT = 5555
+
+locales_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), "locales")
+LOCALES = {
+    "ru_RU.UTF-8": gettext.translation("CHESS", locales_dir, languages=["ru_RU.UTF-8"]),
+    "en_US.UTF-8": gettext.NullTranslations(),
+}
